f9b2ffe4056244bbb521c7e534c93f746ae3be92
ScopellitiValentino <146491115+ScopellitiValentino@users.noreply.github.com>
2024-06-07 00:51:49 -0300

    Delete src/main directory

--- src/main/java/maquina_enigma/web/service/ReflectorService.java
+++
@@ -1,7 +0,0 @@
-package maquina_enigma.web.service;
-import maquina_enigma.web.model.ReflectorInstance;
-
-public interface ReflectorService {
-    public ReflectorInstance createReflector();
-    public Integer reflectar(ReflectorInstance reflectorInstance,Integer position);
-}
